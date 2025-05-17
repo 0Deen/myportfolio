6cb60d2a6b01ff9aae1ba2f21cd2d75e7bfeac0a
Kiprotich Kipchumba <01kiprotich@gmail.com>
2025-05-17 08:31:09 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const botResponse = {
         initialGreeting: "Hi there! I'm Deen. How can I help you today?",
         questionResponse: "Please contact me at +254719760996. If you have another question, feel free to email me at kiprotich66kipchumba@gmail.com.",
-        persistentResponse: "Since you persist, here are my profiles: LinkedIn - https://www.linkedin.com/in/kipchumba-kiprotich-2b10b5279/, GitHub - https://github.com/DeenRamah, Facebook - https://www.facebook.com/profile.php?id=100088239297829"
+        persistentResponse: "Since you persist, here are my profiles: LinkedIn - https://www.linkedin.com/in/kipchumba-kiprotich-2b10b5279/, GitHub - https://github.com/0Deen, Facebook - https://www.facebook.com/profile.php?id=100088239297829"
     };
 
     const chatBox = document.querySelector('.chat-box');
